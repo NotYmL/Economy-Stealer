d34522d400775b408c69fd3fa56b875b879412c5
YmL <72396404+NotYmL@users.noreply.github.com>
2022-03-26 17:55:29 +0100

    Update es.py

--- es.py
+++ es.py
@@ -1,97 +1,68 @@
 import requests, json, sys, os
 from colorama import Fore
 
-with open("config.json") as configFile:
-    jsonObject = json.load(configFile)
-    configFile.close()
-
-token = jsonObject['token']
-prefix = jsonObject['prefix']
-channelId = jsonObject['channelId']
-userId = jsonObject['userId']
-url =  f"https://discord.com/api/v9/channels/{channelId}/messages"
-argument = sys.argv[1]
 green = Fore.GREEN
 reset = Fore.RESET
 
 if os.name == "nt":
     os.system("cls")
-    print(f"""
-{green}    
-
- ________                                                                         ______    __                          __                     
-/        |                                                                       /      \  /  |                        /  |                    
-$$$$$$$$/   _______   ______   _______    ______   _____  ____   __    __       /$$$$$$  |_$$ |_     ______    ______  $$ |  ______    ______  
-$$ |__     /       | /      \ /       \  /      \ /     \/    \ /  |  /  |      $$ \__$$// $$   |   /      \  /      \ $$ | /      \  /      \ 
-$$    |   /$$$$$$$/ /$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$ $$$$  |$$ |  $$ |      $$      \$$$$$$/   /$$$$$$  | $$$$$$  |$$ |/$$$$$$  |/$$$$$$  |
-$$$$$/    $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |       $$$$$$  | $$ | __ $$    $$ | /    $$ |$$ |$$    $$ |$$ |  $$/ 
-$$ |_____ $$ \_____ $$ \__$$ |$$ |  $$ |$$ \__$$ |$$ | $$ | $$ |$$ \__$$ |      /  \__$$ | $$ |/  |$$$$$$$$/ /$$$$$$$ |$$ |$$$$$$$$/ $$ |      
-$$       |$$       |$$    $$/ $$ |  $$ |$$    $$/ $$ | $$ | $$ |$$    $$ |      $$    $$/  $$  $$/ $$       |$$    $$ |$$ |$$       |$$ |      
-$$$$$$$$/  $$$$$$$/  $$$$$$/  $$/   $$/  $$$$$$/  $$/  $$/  $$/  $$$$$$$ |       $$$$$$/    $$$$/   $$$$$$$/  $$$$$$$/ $$/  $$$$$$$/ $$/       
-                                                                /  \__$$ |                                                                     
-                                                                $$    $$/                                                                      
-                                                                 $$$$$$/                                                                       
-                                
-{reset}
-    """)
 else:
     os.system("clear")
-    print(f"""
-{green}    
 
- ________                                                                         ______    __                          __                     
-/        |                                                                       /      \  /  |                        /  |                    
-$$$$$$$$/   _______   ______   _______    ______   _____  ____   __    __       /$$$$$$  |_$$ |_     ______    ______  $$ |  ______    ______  
-$$ |__     /       | /      \ /       \  /      \ /     \/    \ /  |  /  |      $$ \__$$// $$   |   /      \  /      \ $$ | /      \  /      \ 
+print(f"""{green}
+ ________                                                                         ______    __                          __
+/        |                                                                       /      \  /  |                        /  |
+$$$$$$$$/   _______   ______   _______    ______   _____  ____   __    __       /$$$$$$  |_$$ |_     ______    ______  $$ |  ______    ______
+$$ |__     /       | /      \ /       \  /      \ /     \/    \ /  |  /  |      $$ \__$$// $$   |   /      \  /      \ $$ | /      \  /      \
 $$    |   /$$$$$$$/ /$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$ $$$$  |$$ |  $$ |      $$      \$$$$$$/   /$$$$$$  | $$$$$$  |$$ |/$$$$$$  |/$$$$$$  |
-$$$$$/    $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |       $$$$$$  | $$ | __ $$    $$ | /    $$ |$$ |$$    $$ |$$ |  $$/ 
-$$ |_____ $$ \_____ $$ \__$$ |$$ |  $$ |$$ \__$$ |$$ | $$ | $$ |$$ \__$$ |      /  \__$$ | $$ |/  |$$$$$$$$/ /$$$$$$$ |$$ |$$$$$$$$/ $$ |      
-$$       |$$       |$$    $$/ $$ |  $$ |$$    $$/ $$ | $$ | $$ |$$    $$ |      $$    $$/  $$  $$/ $$       |$$    $$ |$$ |$$       |$$ |      
-$$$$$$$$/  $$$$$$$/  $$$$$$/  $$/   $$/  $$$$$$/  $$/  $$/  $$/  $$$$$$$ |       $$$$$$/    $$$$/   $$$$$$$/  $$$$$$$/ $$/  $$$$$$$/ $$/       
-                                                                /  \__$$ |                                                                     
-                                                                $$    $$/                                                                      
-                                                                 $$$$$$/                                                                       
-                                
+$$$$$/    $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |       $$$$$$  | $$ | __ $$    $$ | /    $$ |$$ |$$    $$ |$$ |  $$/
+$$ |_____ $$ \_____ $$ \__$$ |$$ |  $$ |$$ \__$$ |$$ | $$ | $$ |$$ \__$$ |      /  \__$$ | $$ |/  |$$$$$$$$/ /$$$$$$$ |$$ |$$$$$$$$/ $$ |
+$$       |$$       |$$    $$/ $$ |  $$ |$$    $$/ $$ | $$ | $$ |$$    $$ |      $$    $$/  $$  $$/ $$       |$$    $$ |$$ |$$       |$$ |
+$$$$$$$$/  $$$$$$$/  $$$$$$/  $$/   $$/  $$$$$$/  $$/  $$/  $$/  $$$$$$$ |       $$$$$$/    $$$$/   $$$$$$$/  $$$$$$$/ $$/  $$$$$$$/ $$/
+                                                                /  \__$$ |
+                                                                $$    $$/
+                                                                 $$$$$$/
 {reset}
-    """)
+""")
 
-if argument == "usage":
-    print(f"{green}Usage: python app.py <method>")
-    exit()
-elif argument == "methods":
-    print(f"""{green}List of methods you can use:
-    
-    1. collect
-    2. givemoney
-    3. withall
-    4. depall
-    {reset}""")
-    exit()
-elif argument == "collect":
-    collectData = {
-        "content": f"{prefix}collect" 
+authorization = input(f'{green}Token: ')
+prefix = input(f'{green}BOT Prefix: ')
+channelId = input(f'{green}Channel ID: ')
+userId = input(f'{green}User ID: ')
+discordApiV = input(f'{green}[v9] API version: ')
+url =  f"https://discord.com/api/{discordApiV}/channels/{channelId}/messages"
+print(f"""{green}List of methods you can use:
+
+1. collect
+2. givemoney
+3. withall
+4. depall
+{reset}""")
+method = input(f'{green}method: ')
+
+def sendmsg(content):
+    headers={
+        "authorization":f"{authorization}",
+        "content-type": "application/json"
     }
-    collectRequest = requests.post(url=url, headers={"authorization":f"{token}", "content-type": "application/json"}, json=collectData)
-    print(f"{green}Have a nice day :){reset}")
-    exit()
-elif argument == "givemoney":
-    giveMoneyData = {
-        "content": f"{prefix}give <@{userId}> all" 
+
+    JsonPayload = {
+        "content": content
     }
-    giveMoneyRequest = requests.post(url=url, headers={"authorization":f"{token}", "content-type": "application/json"}, json=giveMoneyData)
+    r = requests.post(url=url, headers=headers, json=JsonPayload)
+    print('-----------------------------------------------')
+    print(r.text)
+    print('-----------------------------------------------')
     print(f"{green}Have a nice day :){reset}")
-    exit()
-elif argument == "withall":
-    withallData = {
-        "content": f"{prefix}with all" 
-    }
-    withallRequest = requests.post(url=url, headers={"authorization":f"{token}", "content-type": "application/json"}, json=withallData)
-    print(f"{green}Have a nice day :){reset}")
-    exit()
-elif argument == "depall":
-    depallData = {
-        "content": f"{prefix}dep all" 
-    }
-    depallRequest = requests.post(url=url, headers={"authorization":f"{token}", "content-type": "application/json"}, json=depallData)
-    print(f"{green}Have a nice day :){reset}")
-    exit()
+
+if(method.lower()=='collect' or method.lower()=='1'):
+    sendmsg(content=f"{prefix}collect")
+
+elif(method.lower()=='givemoney' or method.lower()=='2'):
+    sendmsg(content=f"{prefix}give <@{userId}> all")
+
+elif(method.lower()=='withall' or method.lower()=='3'):
+    sendmsg(content=f"{prefix}with all")
+
+elif(method.lower()=='depall' or method.lower()=='4'):
+    sendmsg(content=f"{prefix}dep all")
